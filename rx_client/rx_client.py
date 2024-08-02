36f3a16b8a92a96f766e7b22fc904cd8603c202d
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-08-02 19:16:14 +0300

    Updates to app theme

--- rx_client/rx_client.py
+++ rx_client/rx_client.py
@@ -5,9 +5,4 @@ import reflex as rx
 from rx_client.pages import *
 
 
-APP = {
-    "_light": {"bg": "#f0fff0"},
-    "_dark": {"bg": "#1a1a1a"},
-}
-
-app = rx.App(style=APP, theme=rx.theme(appearance="dark"))
+app = rx.App(theme=rx.theme(appearance="dark"))
